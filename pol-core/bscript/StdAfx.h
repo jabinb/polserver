9244b5f37d622e474624eb9e4c691019a34663e7
Eric Swanson <eric.the.unicorn@gmail.com>
2020-05-08 19:52:36 -0700

    remove everything from bscript/StdAfx.h

--- pol-core/bscript/StdAfx.h
+++ pol-core/bscript/StdAfx.h
@@ -11,52 +11,12 @@
 #if !defined( _BSCRIPT_STDAFX_H )
 #define _BSCRIPT_STDAFX_H
 
-// Copied from clib/StdAfx.h
-//    - except Header_Windows.h which does not play well with ANTLR.
+// include default pch
+#include "../clib/StdAfx.h"
 
-// System Includes
-#include <assert.h>
-#include <atomic>
-#include <cstring>
-#include <deque>
-#include <iostream>
-#include <map>
-#include <memory>
-#include <mutex>
-#include <queue>
-#include <set>
-#include <stack>
-#include <stddef.h>
-#include <stdlib.h>
-#include <string>
-#include <thread>
-#include <vector>
-
-// 3rd Party Includes
-#include <format/format.h>
-#include <boost/algorithm/string/case_conv.hpp>
-#include <boost/any.hpp>
-#include <boost/flyweight.hpp>
-
-// Project Includes (be really really carefull what to include!)
-
-// explicit included since needed anyway for later includes
-#include "../clib/compilerspecifics.h"
-#include "../clib/Debugging/LogSink.h"
-#include "../clib/message_queue.h"
-
-#include "../clib/fixalloc.h"
-#include "../clib/logfacility.h"
-#include "../clib/passert.h"
-#include "../clib/rawtypes.h"
-#include "../clib/refptr.h"
-#include "../clib/spinlock.h"
-#include "../clib/strutil.h"
-#include "../clib/weakptr.h"
-
-// unique to bscript:
 #include "bobject.h"
 #include "token.h"
 #include "tokens.h"
+
 
 #endif
